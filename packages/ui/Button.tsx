2fca000872a063059efe1b4845313d85f635c217
zephyrmathias <wuttichai.junsoontraporn@gmail.com>
2022-08-12 23:55:51 +0700

    test semver

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 export const Button = () => {
-  return <button>This is new version 1.0.0</button>;
+  return <button>This is new version 2.1.0</button>;
 };
--- packages/ui/CHANGELOG.md
+++ packages/ui/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @zephyrmathias2/ui
 
+## 2.1.0
+
+### Minor Changes
+
+- minor bump
+
 ## 2.0.0
 
 ### Major Changes
