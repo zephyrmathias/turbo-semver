9ff34fc0e388dd8b6353e2fe397190efd7516ce9
zephyrmathias <wuttichai.junsoontraporn@gmail.com>
2022-08-12 23:26:18 +0700

    use npm version on web (npm publish)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require("next-transpile-modules")(["ui"]);
+const withTM = require("next-transpile-modules")(["@zephyrmathias2/ui"]);
 
 module.exports = withTM({
   reactStrictMode: true,
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
 export const Button = () => {
-  return <button>Boop</button>;
+  return <button>This is new version 1.0.0</button>;
 };
